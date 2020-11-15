e080a444079b824b3dc49d4a31d07cb9a6b10cf6
usmansbk <usmansbk@gmail.com>
2020-11-15 20:56:15 +0100

    pan responder

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import TwitterHeader from './components/TwitterHeader';
-import AnimatedTuts from './components/AnimatedTut';
+// import TwitterHeader from './components/TwitterHeader';
+// import AnimatedTuts from './components/AnimatedTut';
+import PanResponder from './components/PanResponder';
 
 export default function App() {
-  return <AnimatedTuts />;
+  return <PanResponder />;
 }
