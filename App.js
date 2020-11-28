f1a682465081038cbec2d0b2c5f6f24fd034cfc0
usmansbk <usmansbk@gmail.com>
2020-11-28 16:46:32 +0100

    chore

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 // import TwitterHeader from './components/TwitterHeader';
 // import AnimatedTuts from './components/AnimatedTut';
 // import PanResponder from './components/PanResponder';
-// import Animations from './components/Animations';
+import Animations from './components/Animations';
 import Transformations from './components/Transformation';
 
 export default function App() {
-  return <Transformations />;
+  return <Animations />;
 }
