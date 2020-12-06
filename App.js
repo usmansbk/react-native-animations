5a774c597bf943b5ce9893b7f76cda449b9201e6
Usman Suleiman <usmansbk@gmail.com>
2020-12-07 00:23:34 +0100

    cats tinder

--- App.js
+++ App.js
@@ -5,14 +5,14 @@ import React from 'react';
 // import Transformations from './components/Transformation';
 // import Animations from './components/Animations';
 // import PanResponderTest from './components/PanResponderTest';
-// import KittenCards from './components/KittenCards';
+import KittenCards from './components/KittenCards';
 // import Scroll from './components/Scroll';
 // import HiddenAnimation from './components/Hidden';
 // import InterruptedAnimation from './components/InterruptedAnimation';
 // import Pointer from './components/Pointer';
 // import Corners from './components/Corners';
-import ChatHeads from './components/Heads';
+// import ChatHeads from './components/Heads';
 
 export default function App() {
-  return <ChatHeads />;
+  return <KittenCards />;
 }
--- components/KittenCards.js
+++ components/KittenCards.js
@@ -1,35 +1,92 @@
-import React from 'react';
-import {View, StyleSheet, PanResponder, Animated, Text} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Animated,
+  Dimensions,
+  StyleSheet,
+  View,
+  Text,
+  PanResponder,
+  TouchableOpacity,
+} from 'react-native';
 
-const images = [
-  {
-    id: 1,
-    image:
-      'https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    text: 'Sweet Cat',
-  },
-  {
-    id: 2,
-    image:
-      'https://images.pexels.com/photos/33492/cat-red-cute-mackerel.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    text: 'Sweeter Cat',
-  },
-  {
-    id: 3,
-    image:
-      'https://images.pexels.com/photos/982299/pexels-photo-982299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    text: 'Sweetest Cat',
-  },
-];
+function clamp(value, a, b) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
 
-const SWIPE_THRESHOLD = 120;
+  if (value < min) {
+    return min;
+  } else if (value > max) {
+    return max;
+  }
+  return value;
+}
 
-function clamp() {}
+const {width} = Dimensions.get('window');
+const SWIPE_THRESHOLD = width * 0.3;
+
 export default function KittenCards() {
-  const animation = React.useRef(new Animated.ValueXY()).current;
-  const nextAnimation = React.useRef(new Animated.Value(0.9)).current;
+  const [items, setItems] = useState([
+    {
+      image: 'https://images.pexels.com/photos/982865/pexels-photo-982865.jpeg',
+      id: 1,
+      text: 'Sweet Cat',
+    },
+    {
+      image: 'https://images.pexels.com/photos/991831/pexels-photo-991831.jpeg',
+      id: 2,
+      text: 'Sweeter Cat',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/3777622/pexels-photo-3777622.jpeg',
+      id: 3,
+      text: 'Sweetest Cat',
+    },
+    {
+      image:
+        'https://images.pexels.com/photos/1573324/pexels-photo-1573324.jpeg',
+      id: 4,
+      text: 'Awww',
+    },
+  ]);
+  const animation = useRef(new Animated.ValueXY()).current;
+  const opacity = useRef(new Animated.Value(1)).current;
+  const next = useRef(new Animated.Value(0.9)).current;
 
-  const panResponder = React.useRef(
+  const transitionNext = () => {
+    Animated.parallel([
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.spring(next, {
+        toValue: 1,
+        friction: 4,
+        useNativeDriver: false,
+      }),
+    ]).start(() => {
+      setItems((currentItems) => currentItems.slice(1));
+      next.setValue(0.9);
+      opacity.setValue(1);
+      animation.setValue({x: 0, y: 0});
+    });
+  };
+
+  const handleNo = () => {
+    Animated.timing(animation.x, {
+      toValue: -SWIPE_THRESHOLD,
+      useNativeDriver: false,
+    }).start(transitionNext);
+  };
+  const handleYes = () => {
+    Animated.timing(animation.x, {
+      toValue: SWIPE_THRESHOLD,
+      useNativeDriver: false,
+    }).start(transitionNext);
+  };
+
+  const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onMoveShouldSetPanResponder: () => true,
@@ -43,7 +100,7 @@ export default function KittenCards() {
         ],
         {useNativeDriver: false},
       ),
-      onPanResponderRelease: (evt, {dx, vx, vy}) => {
+      onPanResponderRelease: (_, {dx, vx, vy}) => {
         let velocity;
 
         if (vx >= 0) {
@@ -57,10 +114,10 @@ export default function KittenCards() {
             velocity: {x: velocity, y: vy},
             deceleration: 0.98,
             useNativeDriver: false,
-          });
+          }).start(transitionNext);
         } else {
           Animated.spring(animation, {
-            toValue: {x: 0, y: 0},
+            toValue: 0,
             friction: 4,
             useNativeDriver: false,
           }).start();
@@ -69,42 +126,73 @@ export default function KittenCards() {
     }),
   ).current;
 
+  const rotate = animation.x.interpolate({
+    inputRange: [-200, 0, 200],
+    outputRange: ['-30deg', '0deg', '30deg'],
+    extrapolate: 'clamp',
+  });
+
+  const imageOpacity = animation.x.interpolate({
+    inputRange: [-200, 0, 200],
+    outputRange: [0.5, 1, 0.5],
+    extrapolate: 'clamp',
+  });
+
   const animatedCardStyles = {
-    transform: [
-      {
-        rotate: animation.x.interpolate({
-          inputRange: [-200, 0, 200],
-          outputRange: ['-30deg', '0deg', '30deg'],
-          extrapolate: 'clamp',
-        }),
-      },
-      ...animation.getTranslateTransform(),
-    ],
+    transform: [{rotate}, ...animation.getTranslateTransform()],
+    opacity,
   };
 
-  const animatedImageStyles = {
-    opacity: animation.x.interpolate({
-      inputRange: [-200, 0, 200],
-      outputRange: [0.5, 1, 0.5],
-      extrapolate: 'clamp',
-    }),
+  const animatedImagetyles = {
+    opacity: imageOpacity,
+  };
+
+  const yesOpacity = animation.x.interpolate({
+    inputRange: [0, 150],
+    outputRange: [0, 1],
+    extrapolate: 'clamp',
+  });
+  const yesScale = animation.x.interpolate({
+    inputRange: [0, 150],
+    outputRange: [0.5, 1],
+    extrapolate: 'clamp',
+  });
+  const animatedYupStyles = {
+    transform: [{scale: yesScale}, {rotate: '-30deg'}],
+    opacity: yesOpacity,
+  };
+
+  const noOpacity = animation.x.interpolate({
+    inputRange: [-150, 0],
+    outputRange: [1, 0],
+    extrapolate: 'clamp',
+  });
+  const noScale = animation.x.interpolate({
+    inputRange: [-150, 0],
+    outputRange: [1, 0.5],
+    extrapolate: 'clamp',
+  });
+  const animatedNopeStyles = {
+    transform: [{scale: noScale}, {rotate: '30deg'}],
+    opacity: noOpacity,
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.cards}>
-        {images
+      <View style={styles.top}>
+        {items
           .slice(0, 2)
           .reverse()
-          .map(({image, id, text}, index, items) => {
-            const isLastItem = index === items.length - 1;
-            const isSecondToLast = index === items.length - 2;
+          .map(({id, image, text}, index, cards) => {
+            const isLastItem = index === cards.length - 1;
+            const isSecondToLast = index === cards.length - 2;
 
             const panHandlers = isLastItem ? panResponder.panHandlers : {};
+
+            const imageStyle = isLastItem ? animatedImagetyles : undefined;
             const cardStyle = isLastItem ? animatedCardStyles : undefined;
-            const imageStyle = isLastItem ? animatedImageStyles : undefined;
             const nextStyle = isSecondToLast
-              ? {transform: [{scale: nextAnimation}]}
+              ? {transform: [{scale: next}]}
               : undefined;
 
             return (
@@ -114,17 +202,34 @@ export default function KittenCards() {
                 {...panHandlers}>
                 <Animated.Image
                   source={{uri: image}}
-                  resizeMode="cover"
                   style={[styles.image, imageStyle]}
                 />
-                <View style={styles.footer}>
+                <View style={styles.lowerText}>
                   <Text>{text}</Text>
                 </View>
+
+                {isLastItem && (
+                  <>
+                    <Animated.View style={[styles.nope, animatedNopeStyles]}>
+                      <Text style={styles.nopeText}>Nope!</Text>
+                    </Animated.View>
+                    <Animated.View style={[styles.yup, animatedYupStyles]}>
+                      <Text style={styles.yupText}>Yup!</Text>
+                    </Animated.View>
+                  </>
+                )}
               </Animated.View>
             );
           })}
       </View>
-      <View style={styles.buttonBar}></View>
+      <View style={styles.buttonBar}>
+        <TouchableOpacity style={styles.button} onPress={handleNo}>
+          <Text style={styles.nopeText}>NO</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleYes}>
+          <Text style={styles.yupText}>YES</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -132,12 +237,59 @@ export default function KittenCards() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
   },
-  cards: {
+  top: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  card: {
+    width: 300,
+    height: 300,
+    position: 'absolute',
+    borderRadius: 3,
+    borderWidth: 1,
+    borderColor: '#fff',
+    elevation: 4,
+  },
+  lowerText: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 5,
+  },
+  image: {
+    width: null,
+    height: null,
+    borderRadius: 2,
+    flex: 3,
+  },
+  yup: {
+    borderColor: 'green',
+    borderWidth: 2,
+    position: 'absolute',
+    padding: 20,
+    borderRadius: 5,
+    top: 20,
+    left: 20,
+    backgroundColor: '#fff',
+  },
+  yupText: {
+    fontSize: 16,
+    color: 'green',
+  },
+  nope: {
+    borderColor: 'red',
+    borderWidth: 2,
+    position: 'absolute',
+    padding: 20,
+    borderRadius: 5,
+    right: 20,
+    top: 20,
+    backgroundColor: '#fff',
+  },
+  nopeText: {
+    fontSize: 16,
+    color: 'red',
   },
   buttonBar: {
     flexDirection: 'row',
@@ -145,24 +297,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 10,
   },
-  card: {
-    width: 300,
-    height: 300,
-    position: 'absolute',
-    borderRadius: 3,
-    elevation: 3,
-    borderWidth: 1,
-    borderColor: 'white',
-  },
-  image: {
-    width: null,
-    height: null,
-    flex: 3,
-    borderRadius: 2,
-  },
-  footer: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 5,
+  button: {
+    marginHorizontal: 10,
+    padding: 20,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    elevation: 4,
   },
 });
