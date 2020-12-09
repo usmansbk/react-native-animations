e04d34492ee2b822b75aec80e4f0288bc25d941e
Usman Suleiman <usmansbk@gmail.com>
2020-12-09 01:12:19 +0100

    adhoc touchable opacity

--- components/DirectManipulation.js
+++ components/DirectManipulation.js
@@ -1,23 +1,83 @@
 import React, {useRef} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {
+  Animated,
+  View,
+  Text,
+  StyleSheet,
+  PanResponder,
+  TextInput,
+} from 'react-native';
 
-const MyButton = (props) => {
-  const _root = useRef(null);
+class AdhocTouchableOpacity extends React.Component {
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      this.setOpacityTo(0.5);
+    },
+    onPanResponderRelease: () => {
+      this.setOpacityTo(1);
+      this.props?.onPress();
+    },
+  });
 
-  const setNativeProps = (nativeProps) => {
-    _root.current.setNativeProps(nativeProps);
-  };
+  setOpacityTo(value) {
+    this.CHILD_REF.setNativeProps({
+      opacity: value,
+    });
+  }
+
+  render() {
+    return (
+      <Animated.View
+        {...this.panResponder.panHandlers}
+        ref={(childRef) => (this.CHILD_REF = childRef)}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
+export default () => {
+  const textInput = useRef(null);
   return (
-    <View style={{marginTop: 50}} ref={_root} {...props}>
-      <Text>{props.label}</Text>
+    <View style={styles.container}>
+      <TextInput
+        ref={textInput}
+        style={styles.textInput}
+        placeholder="Enter your text"
+      />
+      <AdhocTouchableOpacity
+        onPress={() => textInput.current.setNativeProps({text: ''})}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Clear text</Text>
+        </View>
+      </AdhocTouchableOpacity>
     </View>
   );
 };
 
-export default () => {
-  return (
-    <TouchableOpacity>
-      <MyButton label="Press me!" />
-    </TouchableOpacity>
-  );
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 16,
+    margin: 4,
+    backgroundColor: 'tomato',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    height: 40,
+    width: 250,
+    paddingHorizontal: 8,
+  },
+});
