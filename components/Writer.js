c9d7fdf659285bd187f223193d59163c9251f184
Usman Suleiman <usmansbk@gmail.com>
2020-12-13 18:06:16 +0100

    editor

--- components/Writer.js
+++ components/Writer.js
@@ -7,10 +7,12 @@ import {
   Dimensions,
   Animated,
   Text,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const {width} = Dimensions.get('window');
+const WIDTH = width * 0.8;
 
 const IconButton = ({name, size = 30, squared}) => {
   const SIZE = squared ? size - 10 : size;
@@ -37,25 +39,90 @@ export default function Writer() {
   const textInputRef = useRef(null);
   const [isOpen, setOpen] = useState(false);
 
+  const toggleTransform = () => {
+    const toValue = isOpen ? 0 : 1;
+
+    Animated.timing(animation, {
+      toValue,
+      duration: 550,
+      useNativeDriver: false,
+    }).start(() => {
+      isOpen ? textInputRef.current.blur() : textInputRef.current.focus();
+      setOpen((prev) => !prev);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.center}>
-        <Animated.View style={styles.editor}>
+        <Animated.View
+          style={[
+            styles.editor,
+            {
+              width: animation.interpolate({
+                inputRange: [0, 0.5],
+                outputRange: [100, WIDTH],
+                extrapolate: 'clamp',
+              }),
+            },
+          ]}>
           <View style={styles.bar}>
-            <Animated.View style={[styles.toolbar]}>
-              <IconButton name="format-bold" />
-              <IconButton name="format-underline" />
-              <IconButton name="format-italic" />
-              <IconButton name="format-list-bulleted" />
-              <IconButton name="format-list-numbered" />
-              <View style={styles.right}>
-                <IconButton name="insert-link" />
-                <IconButton name="insert-photo" />
-                <IconButton name="keyboard-arrow-down" squared />
-              </View>
+            <Animated.ScrollView contentContainerStyle={styles.scrollView}>
+              <Animated.View
+                style={[
+                  styles.toolbar,
+                  {
+                    opacity: animation.interpolate({
+                      inputRange: [0, 0.5],
+                      outputRange: [0, 1],
+                      extrapolate: 'clamp',
+                    }),
+                  },
+                ]}>
+                <IconButton name="format-bold" />
+                <IconButton name="format-underline" />
+                <IconButton name="format-italic" />
+                <IconButton name="format-list-bulleted" />
+                <IconButton name="format-list-numbered" />
+                <View style={styles.right}>
+                  <IconButton name="insert-link" />
+                  <IconButton name="insert-photo" />
+                  <IconButton name="keyboard-arrow-down" squared />
+                </View>
+              </Animated.View>
+            </Animated.ScrollView>
+
+            <Animated.View
+              pointerEvents={isOpen ? 'none' : 'auto'}
+              style={[
+                StyleSheet.absoluteFill,
+                styles.center,
+                {
+                  opacity: animation.interpolate({
+                    inputRange: [0, 0.5],
+                    outputRange: [1, 0],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ]}>
+              <TouchableWithoutFeedback onPress={toggleTransform}>
+                <View>
+                  <Text style={styles.buttonText}>Write</Text>
+                </View>
+              </TouchableWithoutFeedback>
             </Animated.View>
           </View>
-          <Animated.View style={[styles.textInputContainer]}>
+          <Animated.View
+            style={[
+              styles.textInputContainer,
+              {
+                height: animation.interpolate({
+                  inputRange: [0.7, 1],
+                  outputRange: [0, 150],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ]}>
             <TextInput
               placeholder="Start writing..."
               multiline
@@ -65,6 +132,23 @@ export default function Writer() {
           </Animated.View>
         </Animated.View>
       </View>
+      <Animated.View
+        style={[
+          styles.center,
+          {
+            transform: [
+              {
+                scale: animation,
+              },
+            ],
+          },
+        ]}>
+        <TouchableWithoutFeedback onPress={toggleTransform}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Done</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </Animated.View>
     </View>
   );
 }
@@ -78,6 +162,12 @@ const styles = StyleSheet.create({
   center: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  editor: {
+    width: WIDTH,
+  },
+  scrollView: {
+    flexGrow: 1,
   },
   iconButton: {
     alignItems: 'center',
@@ -102,9 +192,6 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     paddingRight: 4,
   },
-  editor: {
-    width: width * 0.8,
-  },
   textInputContainer: {
     height: 150,
     elevation: 4,
@@ -113,5 +200,13 @@ const styles = StyleSheet.create({
   },
   textInput: {
     fontSize: 20,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  button: {
+    margin: 20,
+    padding: 16,
+    backgroundColor: '#2979FF',
   },
 });
